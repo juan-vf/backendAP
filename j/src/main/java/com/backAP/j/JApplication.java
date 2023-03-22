9c26d8cd2e0b132b2863417c33da791e2bb55519
unknown <juanvillan777@gmail.com>
2023-03-22 03:11:17 -0300

    commit for push initializaer project

---
+++ j/src/main/java/com/backAP/j/JApplication.java
@@ -0,0 +1,13 @@
+package com.backAP.j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JApplication.class, args);
+	}
+
+}
